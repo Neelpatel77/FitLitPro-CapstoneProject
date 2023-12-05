081281ff01bbcd8edc35921403185c7e271476e1
Neel Patel <94647177+Neelpatel77@users.noreply.github.com>
2023-12-05 00:27:07 -0500

    Major UI/UX , Graph Calculation , stability update-  #5

--- src/components/ProgressTracker.js
+++ src/components/ProgressTracker.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, FlatList, KeyboardAvoidingView, ScrollView, StyleSheet } from 'react-native';
 import { VictoryLine, VictoryChart, VictoryAxis } from 'victory-native';
 import { VictoryLabel } from 'victory-native';
+import { Picker } from '@react-native-picker/picker'; 
+
 
 import { LineChart } from 'react-native-chart-kit';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -12,6 +14,8 @@ const ProgressTracker = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [measurement, setMeasurement] = useState('');
   const [value, setValue] = useState('');
+  const [selectedUnit, setSelectedUnit] = useState('');
+
 
   useEffect(() => {
     const loadData = async () => {
@@ -38,21 +42,33 @@ const ProgressTracker = () => {
     saveData();
   }, [dataEntries]);
 
+   
   const saveEntry = () => {
-    if (measurement.trim() === '' || value.trim() === '') {
-      alert('Please fill in both fields.');
+    if (measurement.trim() === '' || value.trim() === '' || selectedUnit === '') {
+      alert('Please fill in all fields and select a unit.');
       return;
     }
-    if (isNaN(value) || value.trim() === '') {
+    if (isNaN(value)) {
       alert('Please enter a valid number for the value.');
       return;
     }
-
-    const newEntry = { date: selectedDate.getTime(), measurement, value: parseFloat(value) };
+    if (selectedUnit === '') {
+      alert('Please select the measurement unit between cm and kg.');
+      return;
+    }
+  
+    const newEntry = {
+      date: selectedDate.getTime(),
+      measurement,
+      value: parseFloat(value),
+      unit: selectedUnit, // Include the selected unit
+    };
     setDataEntries([...dataEntries, newEntry]);
     setMeasurement('');
     setValue('');
+    setSelectedUnit(''); // Reset the unit selection
   };
+  
 
   const handleValueChange = (text) => {
     if (/^\d*\.?\d*$/.test(text)) { // Allows only numbers and decimal point
@@ -114,6 +130,25 @@ const ProgressTracker = () => {
               keyboardType="numeric"
               placeholder="Enter Value"
             />
+<View style={styles.pickerContainer}>
+  <Text style={styles.pickerLabel}>Select Unit:</Text>
+ 
+ 
+ 
+ 
+  <Picker
+  selectedValue={selectedUnit}
+  style={styles.picker}
+  onValueChange={(itemValue, itemIndex) => setSelectedUnit(itemValue)}
+  itemStyle={styles.itemStyle} // Apply the style here
+>
+  <Picker.Item label="Please select a unit" value="" />
+  <Picker.Item label="Kilograms (kg)" value="kg" />
+  <Picker.Item label="Centimeters (cm)" value="cm" />
+</Picker>
+
+</View>
+
             <TouchableOpacity
               style={styles.saveButton}
               onPress={saveEntry}
@@ -126,40 +161,52 @@ const ProgressTracker = () => {
         keyExtractor={(_, index) => index.toString()}
         renderItem={({ item, index }) => (
           <View style={styles.dataEntry}>
-            <Text>{new Date(item.date).toLocaleDateString()} - {item.measurement}: {item.value}</Text>
+            <Text>{`${new Date(item.date).toLocaleDateString()} - ${item.measurement} (${item.unit}) - ${item.value}`}</Text>
             <TouchableOpacity onPress={() => deleteEntry(index)}>
               <Text style={styles.deleteText}>Delete</Text>
             </TouchableOpacity>
           </View>
         )}
+        
         ListFooterComponent={
           <View style={styles.chartContainer}>
             <ScrollView horizontal>
+           
+           
             <VictoryChart
   height={500}
   width={800}
   domainPadding={{ x: [30, 30], y: [0, 20] }}
-  padding={{ top: 20, bottom: 50, left: 50, right: 50 }}
+  domain={{ y: [0, 300] }} // Set the domain for y-axis
+
+  padding={{ top: 20, bottom: 50, left: 80, right: 80 }} // Increase left and right padding
 >
+
+
 <VictoryLine
-    data={dataEntries.map(entry => {
-      const numericValue = parseFloat(entry.value);
-      return {
-        x: new Date(entry.date),
-        y: isNaN(numericValue) ? 0 : numericValue,
-        label: `${entry.measurement}: ${numericValue}` // Label for each point
-      };
-    })}
+  data={dataEntries.map(entry => {
+    const numericValue = parseFloat(entry.value);
+    return {
+      x: new Date(entry.date),
+      y: isNaN(numericValue) ? 0 : numericValue,
+      label: `${entry.measurement} (${entry.unit}) - ${numericValue}`, // Include unit in label
+    };
+  })}
     style={{
       data: { stroke: "#c43a31" },
       labels: { fill: "#c43a31", fontSize: 12, padding: 5 }
     }}
-    labelComponent={<VictoryLabel dy={-10} />} // Adjust label position if necessary
+   labelComponent={
+      <VictoryLabel
+        dy={(datum) => (datum.index === 0 ? -15 : datum.index === dataEntries.length - 1 ? 15 : -10)}
+      />
+    }
+    
   />
    <VictoryAxis
     fixLabelOverlap={true}
     style={{
-      tickLabels: { fill: "#006400", fontSize: 12, padding: 5 }, // Dark green color for y-axis labels
+    tickLabels: { fill: "#333", fontSize: 14, padding: 5 }, // Updated style for tick labels on dependent axis
       axisLabel: { padding: 30 },
       grid: { stroke: '#c8d6e5', strokeWidth: 0.25 }
     }}
@@ -174,7 +221,7 @@ const ProgressTracker = () => {
       axisLabel: { padding: 30 }
     }}
     tickFormat={(t) => t % 1 === 0 ? t : ''} // Only show integer ticks
-    tickValues={[5, 10, 15, 20, 25, 30, 35, 40, 45, 50]} // Set this to the range of y-values you expect
+    tickValues={[10, 35, 60, 85, 110, 135, 160, 185, 210, 235]}
  
   />
   
@@ -194,61 +241,85 @@ const ProgressTracker = () => {
 };
 
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 20,
+    padding: 20,
   },
   header: {
-    fontSize: 20,
-    marginBottom: 10,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
   },
   datePickerContainer: {
-    marginBottom: 10,
+    marginBottom: 20,
   },
   datePicker: {
-    width: 200,
+    width: '100%',
   },
   input: {
     borderWidth: 1,
-    padding: 8,
-    marginBottom: 10,
+    borderColor: '#ddd',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 15,
+    fontSize: 16,
   },
   saveButton: {
     backgroundColor: 'blue',
-    padding: 10,
+    padding: 15,
     borderRadius: 5,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   buttonText: {
     color: 'white',
     textAlign: 'center',
-  },
-  buttonText1: {
-    color: '#000080',
-    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   dataEntry: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: 10,
+    paddingVertical: 15,
     borderBottomWidth: 1,
-    borderColor: 'gray',
+    borderColor: '#ddd',
+    marginTop: 20,
   },
   deleteText: {
     color: 'red',
+    fontSize: 16,
   },
   chartContainer: {
     marginTop: 20,
+    marginBottom: 20, // Add space at the bottom of the chart
   },
-  backButton: {
-    backgroundColor: 'green',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
+  picker: {
+    flex: 1,
+    height: 40,
+  },
+  itemStyle: { // Define a separate style for the picker items
+    fontSize: 14, // Set your desired font size for picker items here
+  },
+
+  pickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 120,
+  
+  },
+
+  pickerLabel: {
+    marginTop: 30,
+    fontSize: 15,
+    marginRight: 10,
+  },
+  picker: {
+    flex: 1,
+    height: 10,
+    fontSize: 10,
+    marginBottom: 90,
   },
 });
 
